0f8a0c3c0294a3b8f62285161abacc202b9a77bc
futuregit <tomcis102@hotmail.com>
2021-11-04 21:25:41 -0400

    Started class in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-// Code goes here!
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+
+    constructor() {
+        this.templateElement = document.getElementById('product-input')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importNode = document.importNode(this.templateElement.content, true);
+        this.element = importNode.firstElementChild as HTMLFormElement;
+        this.attach();
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element)
+    }
+}
